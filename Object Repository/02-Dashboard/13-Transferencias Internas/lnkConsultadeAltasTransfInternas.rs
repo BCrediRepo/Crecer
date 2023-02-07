3c5fbee3f78b8f77650d462ab5295d8efa78f9a1
Matias Calderon <Matias.Calderon@tsoftglobal.com>
2023-02-07 15:23:22 -0300

    [FEATURE] TI01-Transferencias Internas
    01-Transferencias Internas. Consulta de Altas Transf. Sucursal Origen. Perfil Planta. Ingresar criterio válido para el que haya registros. Datos válidos

---
+++ Object Repository/02-Dashboard/13-Transferencias Internas/lnkConsultadeAltasTransfInternas.rs
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnkConsultadeAltasTransfInternas</name>
+   <tag></tag>
+   <elementGuidId>50539ef7-f4db-4bcc-a5a5-d656bcef4470</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@onclick=&quot;javascript:menu_history('ENQUIRY','ENQ BCCL.E.CONS.TINT.ALTAS RUN');processMenuHeaderText('Consulta de Altas Transf. Internas')&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@href = concat(&quot;javascript:doenq(&quot; , &quot;'&quot; , &quot;ENQ BCCL.E.CONS.TINT.ALTAS RUN&quot; , &quot;'&quot; , &quot;);&quot;) and (text() = 'Consulta de Altas Transf. Internas ' or . = 'Consulta de Altas Transf. Internas ') and @ref_element = 'Object Repository/02-Dashboard/frmDashboardMenu']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>1efc820f-4d49-4016-8e3b-f0a94ed8ade4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onclick</name>
+      <type>Main</type>
+      <value>javascript:menu_history('ENQUIRY','ENQ BCCL.E.CONS.TINT.ALTAS RUN');processMenuHeaderText('Consulta de Altas Transf. Internas')</value>
+      <webElementGuid>927ee02b-5698-4faa-b027-43650a504bda</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>javascript:doenq('ENQ BCCL.E.CONS.TINT.ALTAS RUN');</value>
+      <webElementGuid>5b23436b-34b1-47cf-8410-bac0843c02be</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Consulta de Altas Transf. Internas </value>
+      <webElementGuid>82749022-ee3f-46af-80ee-28c453de7569</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;pane_&quot;)/ul[@class=&quot;menuMargin&quot;]/li[@class=&quot;clsHasKids&quot;]/ul[1]/li[4]/a[1]</value>
+      <webElementGuid>b609bc40-6e1d-43e4-a961-209fae23c79b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/02-Dashboard/frmDashboardMenu</value>
+      <webElementGuid>2af1107f-4493-4174-a471-a843b964a340</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@onclick=&quot;javascript:menu_history('ENQUIRY','ENQ BCCL.E.CONS.TINT.ALTAS RUN');processMenuHeaderText('Consulta de Altas Transf. Internas')&quot;]</value>
+      <webElementGuid>50bd5e96-e728-4b8b-8fb6-4028bd24beb8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='pane_']/ul[19]/li/ul/li[4]/a</value>
+      <webElementGuid>3cd48096-889f-467a-9c07-75c6e4ecacea</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Consulta de Altas Transf. Internas')]</value>
+      <webElementGuid>6c1c3955-542f-4c45-a13a-bcae62c0759f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pago Transf. Interna - Posteo'])[1]/following::a[1]</value>
+      <webElementGuid>feda6ffe-09bf-400f-8cce-0188126e74c5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Alta Transf. Interna Origen Cuenta'])[1]/following::a[2]</value>
+      <webElementGuid>3662362a-1a7c-4cde-90ff-079b47588b72</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Consulta de Transf. Internas Pagadas'])[1]/preceding::a[1]</value>
+      <webElementGuid>6dfd2865-5683-402d-a483-5825fc61e4c6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ajustes Monetarios'])[1]/preceding::a[2]</value>
+      <webElementGuid>820603ba-d745-453d-8e13-90d1f7e7f769</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Consulta de Altas Transf. Internas']/parent::*</value>
+      <webElementGuid>0e8601ed-d331-4d0f-b37d-abe32ea1f30c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, &quot;javascript:doenq('ENQ BCCL.E.CONS.TINT.ALTAS RUN');&quot;)]</value>
+      <webElementGuid>9c436123-bf3e-4512-9900-48602a65c711</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ul[19]/li/ul/li[4]/a</value>
+      <webElementGuid>e2bb205e-4d2e-414d-96ab-40a30618dd64</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = concat(&quot;javascript:doenq(&quot; , &quot;'&quot; , &quot;ENQ BCCL.E.CONS.TINT.ALTAS RUN&quot; , &quot;'&quot; , &quot;);&quot;) and (text() = 'Consulta de Altas Transf. Internas ' or . = 'Consulta de Altas Transf. Internas ')]</value>
+      <webElementGuid>6ab5f411-5903-4b79-82cc-b2831340a684</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/12-TransferenciasInternas/TI01-Transferencias Internas. Consulta de Altas Transf. Sucursal Origen. Perfil Planta. Ingresar criterio válido para el que haya registros. Datos válidos/Script1675791814194.groovy
@@ -0,0 +1,54 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+//Config
+LocalDateTime now = LocalDateTime.now()
+DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE
+String nowString = formatter.format(now)
+
+//Login
+CustomKeywords.'pkgModules.kywGeneric.Login'(GlobalVariable.vTest10_IP, GlobalVariable.vTest10Name, GlobalVariable.vF00289, GlobalVariable.vPass)
+
+//Se accede al menu Transferencias Internas - Consulta de Altas Transf. Internas
+WebUI.maximizeWindow()
+WebUI.click(findTestObject('Object Repository/02-Dashboard/lnkTransferenciasInternas'))
+WebUI.click(findTestObject('Object Repository/02-Dashboard/13-Transferencias Internas/lnkConsultadeAltasTransfInternas'))
+
+//Se espera la carga de BCCL.E.CONS.TINT.ALTAS
+WebUI.delay(3)
+WebUI.switchToWindowTitle(findTestData('Modulos/Modulos').getValue(4,17))
+WebUI.maximizeWindow()
+
+//Se valida la existencia de objetos en BCCL.E.CONS.TINT.ALTAS
+WebUI.waitForElementVisible(findTestObject('Object Repository/12-Transferencias Internas/pageBCCL.E.CONS.TINT.ALTAS/tdReferencia'),3)
+WebUI.waitForElementVisible(findTestObject('Object Repository/12-Transferencias Internas/pageBCCL.E.CONS.TINT.ALTAS/tdIdOrdenante'),3)
+
+//---------------------------------------------------------------------------------------------------------------------
+//Control de fin de script
+@com.kms.katalon.core.annotation.TearDownIfFailed
+void fTakeFailScreenshot() {
+	CustomKeywords.'pkgModules.kywGeneric.fFailStatus'('Screenshot/Fails/'+ 'TI01' +'.png')
+}
+
+@com.kms.katalon.core.annotation.TearDownIfPassed
+void fPassScript() {
+	CustomKeywords.'pkgModules.kywGeneric.fPassStatus'()
+}
