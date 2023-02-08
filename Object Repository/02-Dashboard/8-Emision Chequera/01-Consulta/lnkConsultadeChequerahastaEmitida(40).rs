162f4fd14ea247211d1b736057cca537d146ded0
Guillermo.Ameztoy <Guillermo.Ameztoy@AR-NB-745.tsoftglobal.com>
2023-02-08 12:55:14 -0300

    [FEATURE] Emision Chequera
    EC01-Emision chequera.Cons. Chequeras Solic. Control de Facturacion
    EC02-Emision Chequera.Invocación del ENQ BCCL.CQ.CONS.ANUL con datos
    válidos para generar autorizaciones
    EC03-Emisión de Chequera.Consulta de chequeras serv adm .Consulta con
    registro cambiado hace más de 3 días

---
+++ Object Repository/02-Dashboard/8-Emision Chequera/01-Consulta/lnkConsultadeChequerahastaEmitida(40).rs
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnkConsultadeChequerahastaEmitida(40)</name>
+   <tag></tag>
+   <elementGuidId>d5256205-a98a-4e7d-91e2-8bc093f0b5a9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@onclick=&quot;javascript:menu_history('ENQUIRY','ENQ BCCL.CQ.CHEQUERAS');processMenuHeaderText('Consulta de Chequera hasta Emitida (40)')&quot;]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>acdf4fa4-a164-4c51-9d94-5b7d77857a91</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onclick</name>
+      <type>Main</type>
+      <value>javascript:menu_history('ENQUIRY','ENQ BCCL.CQ.CHEQUERAS');processMenuHeaderText('Consulta de Chequera hasta Emitida (40)')</value>
+      <webElementGuid>f3c3bf5c-9fc7-4bd1-8faf-29d3a1ae0c62</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>javascript:doenq('ENQ BCCL.CQ.CHEQUERAS');</value>
+      <webElementGuid>aea15b76-1cea-414d-ad37-fc496fb9a5d5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Consulta de Chequera hasta Emitida (40) </value>
+      <webElementGuid>9c3376ba-2ea9-4900-8106-16750228abd5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;pane_&quot;)/ul[@class=&quot;menuMargin&quot;]/li[@class=&quot;clsHasKids&quot;]/ul[1]/li[@class=&quot;clsHasKids&quot;]/ul[1]/li[2]/a[1]</value>
+      <webElementGuid>20fe045b-6b5e-4940-bc6c-e38fb13d4ea2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/02-Dashboard/frmDashboardMenu</value>
+      <webElementGuid>e1b2c4a8-55b8-4b15-8cab-5da6f2ef097f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@onclick=&quot;javascript:menu_history('ENQUIRY','ENQ BCCL.CQ.CHEQUERAS');processMenuHeaderText('Consulta de Chequera hasta Emitida (40)')&quot;]</value>
+      <webElementGuid>ea02ddd0-6107-432f-a67d-6c28b72ef742</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='pane_']/ul[12]/li/ul/li[3]/ul/li[2]/a</value>
+      <webElementGuid>177b80bf-2796-4e0f-b76d-ad6ac6cadce8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Consulta de Chequera hasta Emitida (40)')]</value>
+      <webElementGuid>1f0bcf87-6eae-4ea0-8354-e335855e643c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Consulta por Cheque'])[1]/following::a[1]</value>
+      <webElementGuid>b1508ba3-e162-4913-99b7-19dff00ae433</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Consulta'])[3]/following::a[2]</value>
+      <webElementGuid>b7376644-9a33-4c2d-a957-ba692ca82b0e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Consulta / Conf de Chequeras con Errores (21)'])[1]/preceding::a[1]</value>
+      <webElementGuid>a590f4ff-a394-48aa-a3d6-8b01936d7677</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Consulta de Chequera desde Emitida (40 - 90)'])[1]/preceding::a[2]</value>
+      <webElementGuid>984c6d36-de39-4a7b-8de4-404993be8079</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Consulta de Chequera hasta Emitida (40)']/parent::*</value>
+      <webElementGuid>a7dd6f56-5b44-4a9f-a2c4-b913da9f2b8d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, &quot;javascript:doenq('ENQ BCCL.CQ.CHEQUERAS');&quot;)]</value>
+      <webElementGuid>2acd3ab2-2ffd-48e9-83d1-36de9ce89936</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ul[12]/li/ul/li[3]/ul/li[2]/a</value>
+      <webElementGuid>810383a8-3006-4a26-b3ce-b3ac96112591</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = concat(&quot;javascript:doenq(&quot; , &quot;'&quot; , &quot;ENQ BCCL.CQ.CHEQUERAS&quot; , &quot;'&quot; , &quot;);&quot;) and (text() = 'Consulta de Chequera hasta Emitida (40) ' or . = 'Consulta de Chequera hasta Emitida (40) ')]</value>
+      <webElementGuid>7fbeff6f-031e-4aa8-b1a6-bc6e936558eb</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
