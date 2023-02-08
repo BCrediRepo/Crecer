f620335d829e4fd9e537f21a9c17774805fd170a
Gino Gutierrez <ginogutierrez95@gmail.com>
2023-02-08 11:58:18 -0300

    [FEATURE]Dashboard
    Objetos Dashboard - Comisiones
    Objetos Dashboard - Cheques Rechazados
    Objetos Dashboard - Movimientos Automaticos

---
+++ Object Repository/02-Dashboard/6-Cheques rechazados/1-Consultas - Temenos T24/lnkChequesRechazados.rs
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnkChequesRechazados</name>
+   <tag></tag>
+   <elementGuidId>2c14c83c-0864-4df7-8e81-87b0477da8ed</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@onclick=&quot;javascript:menu_history('ENQUIRY','ENQ BCCL.CHRECH.RECHAZADOS');processMenuHeaderText('Cheques Rechazados')&quot;]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>9c59ce4b-3d99-46ae-ac0d-caab59bd99c3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onclick</name>
+      <type>Main</type>
+      <value>javascript:menu_history('ENQUIRY','ENQ BCCL.CHRECH.RECHAZADOS');processMenuHeaderText('Cheques Rechazados')</value>
+      <webElementGuid>9f5b531f-6887-46a9-9e8f-3bd657e28cd7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>javascript:doenq('ENQ BCCL.CHRECH.RECHAZADOS');</value>
+      <webElementGuid>d374c272-91dc-42b6-9956-2fcdb4a1588e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Cheques Rechazados </value>
+      <webElementGuid>afcc38c3-3190-44a3-9f3d-f22f64f6e6d8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;pane_&quot;)/ul[@class=&quot;menuMargin&quot;]/li[@class=&quot;clsHasKids&quot;]/ul[1]/li[@class=&quot;clsHasKids&quot;]/ul[1]/li[3]/a[1]</value>
+      <webElementGuid>8bb6fdb4-a29d-49d8-9208-c573d0650f7d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@onclick=&quot;javascript:menu_history('ENQUIRY','ENQ BCCL.CHRECH.RECHAZADOS');processMenuHeaderText('Cheques Rechazados')&quot;]</value>
+      <webElementGuid>b124caa3-b3e5-4489-8958-4ca08334eb46</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='pane_']/ul[20]/li/ul/li[7]/ul/li[3]/a</value>
+      <webElementGuid>a59beb0e-cc22-4dc9-91d5-74549e8e4707</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'Cheques Rechazados')])[2]</value>
+      <webElementGuid>57936db7-cbb7-41f3-b696-c97f8b757e58</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Multas pendientes'])[1]/following::a[1]</value>
+      <webElementGuid>17c0c4e6-fb6c-4658-8299-8101b75ad99b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Multas'])[1]/following::a[2]</value>
+      <webElementGuid>867c7c97-74f3-45c0-bfa4-4c2960b3e8a1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Historial Cheque'])[1]/preceding::a[1]</value>
+      <webElementGuid>d80cfd76-b6ab-43a0-8b13-440f1a1cbb54</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cheques Colisionados del Dia'])[1]/preceding::a[2]</value>
+      <webElementGuid>353b2ded-0358-4cfd-9595-6d4b9462a79c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Cheques Rechazados']/parent::*</value>
+      <webElementGuid>db1849b8-f51e-4ad0-8fd8-bab4f20d9abe</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, &quot;javascript:doenq('ENQ BCCL.CHRECH.RECHAZADOS');&quot;)]</value>
+      <webElementGuid>f425da22-b90a-4540-ab17-1ac5f007bcf2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ul[20]/li/ul/li[7]/ul/li[3]/a</value>
+      <webElementGuid>e011ec72-5ce6-466f-b601-d2aaf1eef257</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = concat(&quot;javascript:doenq(&quot; , &quot;'&quot; , &quot;ENQ BCCL.CHRECH.RECHAZADOS&quot; , &quot;'&quot; , &quot;);&quot;) and (text() = 'Cheques Rechazados ' or . = 'Cheques Rechazados ')]</value>
+      <webElementGuid>70f94f1c-d91b-48e7-a971-a3b866611664</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
