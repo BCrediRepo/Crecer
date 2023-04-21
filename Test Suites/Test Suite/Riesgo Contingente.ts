0276fe53cb436f48c2d2a102d46f4d66cd231e61
Guillermo Ameztoy <Guillermo.Ameztoy@tsoftglobal.com>
2023-04-21 12:57:14 -0300

    [FIX] Se agregaron TestSuites a la lista

---
+++ Test Suites/Test Suite/Riesgo Contingente.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Riesgo Contingente</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e5f2365e-f7c3-4f90-90da-145783455730</testSuiteGuid>
+   <testCaseLink>
+      <guid>a6de2916-d4c7-4fb7-9548-ad31925e4083</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/33-Riesgo Contingente/RC01-Consulta comisiones calculadas y cobradas por riesgo contingente. Para el mes y año de la fecha de proceso. Cuenta y Sucursal informada con Bonificación</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Test Suite/Sobregiros.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sobregiros</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f2def691-f33e-431d-8661-54708fff58b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>5f5f84fb-4018-4d98-8f0f-235cdadb391b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/34-Sobregiros/SG01-Sobregiros. Consulta Credicuenta con Excesos</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7e16ced9-24a0-48db-84f2-fe9f72dd469f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/34-Sobregiros/SG02-Sobregiros. Consulta de acuerdos vencidos</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
