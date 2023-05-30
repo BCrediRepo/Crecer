254da51e0b41e0ae5754195622a2c1b7dab15060
Gino Gutierrez <ginogutierrez95@gmail.com>
2023-05-30 13:09:14 -0300

    [FEATURE] Cierre de cuentas s20230515
    CDC05 al CDC08

---
+++ Scripts/24-Cierre de Cuenta/CDC06-Búsqueda de cuentas para cierre con saldo. Búsqueda por Nro de CUI inexistente. Validación/Script1684505471435.groovy
@@ -0,0 +1,70 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import java.time.LocalDateTime as LocalDateTime
+import java.time.format.DateTimeFormatter as DateTimeFormatter
+
+//Configuracion de ambiente
+CustomKeywords.'pkgModules.kywGeneric.ConfigEnvironment'(GlobalVariable.vServerIPRun, GlobalVariable.vServerNameRun)
+
+//Login
+CustomKeywords.'pkgModules.kywGeneric.Login'(findTestData('MainData/Users').getValue(1, 9), findTestData('MainData/Users').getValue(
+        2, 9))
+
+WebUI.click(findTestObject('02-Dashboard/lnkCuentas'))
+
+WebUI.click(findTestObject('02-Dashboard/23-Cierre de Cuenta/lnkCierreDeCuenta'))
+
+WebUI.click(findTestObject('02-Dashboard/23-Cierre de Cuenta/01-Cierre de cuenta/lnkCdCConCancelaciondeSaldo'))
+
+WebUI.switchToWindowIndex(1)
+
+CustomKeywords.'pkgModules.kywGeneric.LimpiarFiltroenScript'()
+
+WebUI.switchToWindowIndex(0)
+
+WebUI.click(findTestObject('02-Dashboard/23-Cierre de Cuenta/01-Cierre de cuenta/lnkCdCConCancelaciondeSaldo'))
+
+WebUI.switchToWindowIndex(1)
+
+if (findTestObject('Object Repository/25-Cierre de Cuenta/BCCL.AC.CIERRE.CTA.CANC.SALDO/lblPos4') == 'No. CUI') {
+    WebUI.setText(findTestObject('25-Cierre de Cuenta/BCCL.AC.CIERRE.CTA.CANC.SALDO/txtPos4'), '9999999999')
+} else {
+	WebUI.setText(findTestObject('25-Cierre de Cuenta/BCCL.AC.CIERRE.CTA.CANC.SALDO/txtPos1'), '9999999999')
+}
+
+WebUI.click(findTestObject('00-Utils/02-Filtros/lnkEjecutar'))
+
+WebUI.verifyElementVisible(findTestObject('25-Cierre de Cuenta/BCCL.AC.CIERRE.CTA.CANC.SALDO/lblRegistront'))
+
+Registront = WebUI.getText(findTestObject('25-Cierre de Cuenta/BCCL.AC.CIERRE.CTA.CANC.SALDO/lblRegistront'))
+
+assert Registront == 'NO SE ENCONTRARON REGISTROS'
+
+WebUI.maximizeWindow()
+
+@com.kms.katalon.core.annotation.TearDownIfFailed
+void fTakeFailScreenshot() {
+    CustomKeywords.'pkgModules.kywGeneric.fFailStatus'()
+}
+
+@com.kms.katalon.core.annotation.TearDownIfPassed
+void fPassScript() {
+    CustomKeywords.'pkgModules.kywGeneric.fPassStatus'()
+}
+
